f7bef8fff7e27f61f57021ccc3c147575061e1cb
callee2006 <ijkim@handong.edu>
2018-08-30 20:25:20 +0900

    initial version

---
+++ Automobile.h
@@ -0,0 +1,12 @@
+class Automobile {
+private:    
+    float fuel;
+    float speed;
+    char licensePlate[32];
+
+public:
+    Automobile();
+    void Display();
+    void Accelerate(int amount);
+    void Decelerate(int amount);
+};
